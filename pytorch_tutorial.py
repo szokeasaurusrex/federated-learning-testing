2c34604b2f4b4c28588da157ad448b12a5d63ec2
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2022-09-30 18:08:01 -0400

    Code from following PyTorch tutorial

---
+++ pytorch_tutorial.py
@@ -0,0 +1,106 @@
+# Code resulting from following PyTorch tutorial here: https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+# Much of the code is copied from the tutorial, or heavily based on it.
+
+import torch
+import torch.backends.mps
+from torch import nn
+from torch.utils.data import DataLoader
+import torchvision
+
+
+class NeuralNetwork(torch.nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.flatten = torch.nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10)
+        )
+    
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+def train(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * len(X)
+            print(f'loss: {loss:>7f} [{current:>5d}/{size:>5d}]')
+
+def test(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+def main():
+    device = 'mps' if torch.backends.mps.is_available() else 'cpu'  # Attempt to use Metal hardware acceleration
+    print(f'Using {device} device')
+
+    training_data = torchvision.datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=torchvision.transforms.ToTensor()
+    )
+
+    test_data = torchvision.datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=True,
+        transform=torchvision.transforms.ToTensor()
+    )
+
+    batch_size = 64
+
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    for X, y in test_dataloader:
+        print(f"Shape of X [N, C, H, W]: {X.shape}")
+        print(f"Shape of y: {y.shape} {y.dtype}")
+        break
+    
+
+    model = NeuralNetwork().to(device)
+    print(model)
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+    epochs = 2
+    for t in range(epochs):
+        print(f'Epoch {t+1}\n----------------------')
+        train(train_dataloader, model, loss_fn, optimizer, device)
+        test(test_dataloader, model, loss_fn, device)
+    
+    # Probably can use state_dict to average models
+    print('done')
+
+
+if __name__ == '__main__':
+    main()
